ec2939e6502e2592ab1856bf067cf2138ed7cfe1
Itzik Pazuelo <itpazu@gmail.com>
2020-03-08 19:35:07 +0200

    milestone 5 completed

--- myjs.js
+++ myjs.js
@@ -1,8 +1,12 @@
+// excersice 2:
+
 // // let x = 7;
 // let y = 11;
 // console.log(x);
 // document.getElementById('x-var').innerText = x;
 // document.getElementById('y-var').innerText = y;
+
+// exercise 3
 
 // let x = 2;
 // let myArray = [0, 1];
@@ -24,44 +28,53 @@
 // const yVar = document.getElementById('yVar');
 // yVar.innerHTML= y;
 
-// function works 1
-// function calcBtnClick(userInput) {
+// exercise 4
+
+// function c() {
+//   let userInput = document.getElementById('inputBox').value;
+//   console.log (userInput);
 //   let y;
 //   let myArray = [0, 1];
-//     for (i = 0; i < userInput; i++) {
-//       const newNum = myArray[i] + myArray[i + 1];
-//       myArray.push(newNum);
-
-//     }
-//     y = myArray[myArray.length - 2];
-//     return y;
-//     console.log(y);
+//   for (i = 0; i < userInput; i++) {
+//     const newNum = myArray[i] + myArray[i + 1];
+//     myArray.push(newNum);
 //   }
+//   y = myArray[myArray.length - 2];
+//   const resultCalc = document.getElementById('resultCalc');
+//   resultCalc.innerHTML = y;
 
+// }
 
+// const calcBtn = document.getElementById('butn');
+// calcBtn.addEventListener('click', c);
 
+// document.getElementById('butn').addEventListener('click', () => {
+//   let userInput = document.getElementById('inputBox').value;
+//   let y;
+//   let myArray = [0, 1];
+//   for (i = 0; i < userInput; i++) {
+//     const newNum = myArray[i] + myArray[i + 1];
+//     myArray.push(newNum);
+//   }
+//   y = myArray[myArray.length - 2];
+//   const resultCalc = document.getElementById('resultCalc');
+//   resultCalc.innerHTML = y;
+// });
 
-function c() {
-  let userInput = document.getElementById('inputBox').value;
-  console.log (userInput);
-  let y;
-  let myArray = [0, 1];
-  for (i = 0; i < userInput; i++) {
-    const newNum = myArray[i] + myArray[i + 1];
-    myArray.push(newNum);
-  }
-  // console.log(myArray);
-  y = myArray[myArray.length - 2];
-  // console.log(y);
-  // return y;
-  const resultCalc = document.getElementById('resultCalc');
-  resultCalc.innerHTML = y;
+const url = 'http://localhost:5050/fibonacci/';
 
-}
+document.getElementById('butn').addEventListener('click', () => {
+  console.log('click');
+  userInput = document.getElementById('inputBox').value;
 
-const calcBtn = document.getElementById('butn');
-calcBtn.addEventListener('click', c);
-
-
-
-
+  fetch(url + userInput)
+    .then(response => response.json())
+    .then(function(data) {
+      let y = date.result;
+      console.log(y);
+      document.getElementById('resultCalc').innerHTML = y;
+    })
+    .catch(function(error) {
+      console.log('error');
+    });
+});
